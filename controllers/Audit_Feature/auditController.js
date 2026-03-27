8c2d641a2632bc8975f3358445cf883e86ab98c1
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-03-27 13:02:23 +0800

    remove logs in audit logs

--- controllers/Audit_Feature/auditController.js
+++ controllers/Audit_Feature/auditController.js
@@ -4,10 +4,8 @@ export const getAuditLogs = async (req , res) => {
   try {
     const logs = await auditService.getAuditLogs();
     res.json(logs);
-    console.log("Audit logs sent:", logs);
   } catch (error) {
     res.status(500).json({error: error.message}); 
-    console.error("Audit logs error:", error);
   };
 };
 
