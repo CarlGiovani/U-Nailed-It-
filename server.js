c17087fc3cd28b9cd7415da0942cba40fef98b94
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-04-08 16:30:09 +0800

    change ip in cors note: change back to local

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://192.168.100.5:5173",
-      "http://192.168.100.5:5174",
+      "http://10.246.128.215:5173",
+      "http://10.246.128.2151:5174",
       "http://localhost:5173",
       "http://localhost:5174",
     ],
