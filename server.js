b0bb8ce4609507d66e91d55d1216c696345f5b44
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-04-12 14:17:13 +0800

    updated

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { scheduleBookingExpiry } from "./utils/bookingExpiryCron.js";
-import { scheduleSlotCleanup } from "./utils/slotCron.js";
 
 // ENDPOINTS IMPORT
 import notificationRoutes from "./routes/Admin_Notification_Feature/notificationRoutes.js";
@@ -10,7 +8,9 @@ import announcementRoutes from "./routes/Announcement_Feature/announcementRoutes
 import auditRoutes from "./routes/Audit_Feature/auditRoutes.js";
 import authRoutes from "./routes/Authentication_Feature/authRoutes.js";
 import bookingRouter from "./routes/Booking_Feature/bookingRoutes.js";
+import bookingRemindersRoutes from "./routes/Booking_Reminders_Feature/bookingRemindersRoutes.js";
 import calendarRoutes from "./routes/Calendar_Feature/calendarRoutes.js";
+import jobRoutes from "./routes/Cron_Jobs_Feature/jobRoutes.js";
 import customerRouter from "./routes/Customer_Feature/customerRoutes.js";
 import dashboardRoutes from "./routes/Dashboard_Feature/dashboardRoutes.js";
 import paymentRouter from "./routes/Payment_Feature/paymentRoutes.js";
@@ -19,8 +19,6 @@ import portfolioRoutes from "./routes/portfolio_Feature/portfolioRoutes.js";
 import reviewsRoutes from "./routes/Review_Feature/reviewsRoutes.js";
 import servicesRouter from "./routes/Services_Feature/serviceRoutes.js";
 import testEmailRoutes from "./routes/testEmail.js";
-import bookingRemindersRoutes from "./routes/Booking_Reminders_Feature/bookingRemindersRoutes.js";
-import jobRoutes from "./routes/Cron_Jobs_Feature/jobRoutes.js";
 
 dotenv.config();
 
@@ -31,8 +29,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://192.168.100.5:5173",
-      "http://192.168.100.5:5174",
+      "http://192.168.240.1:5173",
+      "http://192.168.240.1:5174",
       "http://localhost:5173",
       "http://localhost:5174",
     ],
