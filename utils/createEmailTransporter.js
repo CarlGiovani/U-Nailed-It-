db951a9cf55f430903e991f982bd7c065b63ecac
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-04-30 02:44:16 +0800

    deployment V3

--- utils/createEmailTransporter.js
+++ utils/createEmailTransporter.js
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 
 const createEmailTransporter = ({ email_user, email_app_password }) => {
   return nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: email_user,
       pass: email_app_password,
