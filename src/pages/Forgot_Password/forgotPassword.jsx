e2103ba463f0231a0c02c09b0d76607e9e6957d5
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-04-29 22:47:41 +0800

    DeploymentV3

--- src/pages/Forgot_Password/forgotPassword.jsx
+++ src/pages/Forgot_Password/forgotPassword.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../../../../UNailedIt_Website/src/assets/images/logo.png";
+import logo from "../../assets/logo.png";
 import { forgotPassword } from "../../services/BACKEND/adminAuthApi";
 import "../../styles/AdminLogin.css";
 
