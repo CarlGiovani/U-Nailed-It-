4938a56612e5f7ad122f6b05234579e47251b2f6
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-04-30 01:07:33 +0800

    deploymentV6

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend mo
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", // TAMA ITO KUNG NASA PORT 5000 ANG BACKEND
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 export default api;
